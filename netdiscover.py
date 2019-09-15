db420f9084d8b7f9910c38a1f394d6e7ad74d8df
wildcat <letranhaitung@gmail.com>
2019-09-15 12:02:45 -0400

    add to function

--- netdiscover.py
+++ netdiscover.py
@@ -7,11 +7,11 @@ import ipaddress
 from pprint import pprint
 
 def run():
-    adapters = ifaddr.get_adapters()
+    adapters = list(ifaddr.get_adapters())
 
     print("Choose an adapter:")
     for i in range(len(adapters)):
-        print("{0:>2}) [{1:>18}] {2}".format(i, adapters[i].ips[-1].ip + '/' + str(adapters[i].ips[-1].network_prefix), adapters[i].nice_name))
+        print("{0:>2}) [{1:>18}] {2}".format(i, adapters[i].ips[0].ip + '/' + str(adapters[i].ips[0].network_prefix), adapters[i].nice_name))
 
     choice = int(input(">>> "))
     if choice in range(len(adapters)):
@@ -20,15 +20,19 @@ def run():
         print("Choice not in range.")
         exit(1)
 
-    ip = adapters[choice].ips[-1].ip
-    network_prefix = adapters[choice].ips[-1].network_prefix
+    ip = adapters[choice].ips[0].ip
+    network_prefix = adapters[choice].ips[0].network_prefix
 
     # List all address to scan
     addresses = ipaddress.ip_network(ip + '/' + str(network_prefix), strict=False).hosts()
-    for i in range(10):
-        print(repr(next(addresses)))
 
-def packet_build(addr):
+    # Create socket
+    sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.SOCK_RAW)
+
+    for addr in addresses:
+        data = packet_builder()
+
+def packet_builder(addr):
     """Build an ARP packet with the IP address, return bytes"""
     pass
 
